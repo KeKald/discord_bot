bed5a7d8ecacf1a7a747034702922cda748d98ff
KeKald <kerps12@gmail.com>
2019-01-11 15:56:59 +0200

    Two added broken features

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext.commands import Bot
 import dotenv
 import os
+import requests
 
 dotenv.load_dotenv()
 
@@ -9,7 +10,7 @@ TOKEN = os.getenv("DISCORD_AUTH_TOKEN")
 BOT_PREFIXES = ("!")
 MyBot = Bot(command_prefix = BOT_PREFIXES)
 
-# Startup message
+#Startup message
 @MyBot.event
 async def on_ready():
     print("\nBot active...\n")
@@ -24,10 +25,21 @@ async def hello_world(context):
     channel = context.channel
     await channel.send("Hello world!")
 
-#
-@MyBot.event
-async def message():
 
-    print(message(content))
+#@MyBot.event
+#async def on_message(message):
+#
+#    print(message.content)
+#    
+#    await MyBot.send_message(message.channel, message.content)
+
+@MyBot.command(aliases=["downloadimage", "dimage", "downimage", "dimg"], pass_context = True)
+async def download_image(context):
+
+    test = message.attachments
+    print(test)
+
+    channel = context.channel
+    await channel.send(test)
 
 MyBot.run(TOKEN)

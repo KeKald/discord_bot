095efa447f525783ac0a55e2cce5e43118d4c827
KeKald <kerps12@gmail.com>
2019-01-18 15:43:21 +0200

    Multiple commands added

--- bot.py
+++ bot.py
@@ -9,16 +9,16 @@ dotenv.load_dotenv()
 
 TOKEN = os.getenv("DISCORD_AUTH_TOKEN")
 BOT_PREFIXES = ("!")
-MyBot = Bot(command_prefix = BOT_PREFIXES)
+MyBot = Bot(command_prefix=BOT_PREFIXES)
 
-#Startup message
+# Startup message
 @MyBot.event
 async def on_ready():
     print("\nBot active...\n")
 
 
 # Simple Hello world text
-@MyBot.command(aliases = ["helloworld", "hiworld", "heyworld", "hey", "hi"], pass_context = True)
+@MyBot.command(aliases=["helloworld", "hiworld", "heyworld", "hey", "hi"], pass_context=True)
 async def hello_world(context):
 
     print("\nHello world!")
@@ -26,23 +26,24 @@ async def hello_world(context):
     channel = context.channel
     await channel.send("Hello world!")
 
-#Event which prints out every message sent in channel
-#@MyBot.event
-#async def on_message(message):
+# Event which prints out every message sent in channel
+# @MyBot.event
+# async def on_message(message):
 #
 #    print(message.content)
-#    
+#
 #    await MyBot.send_message(message.channel, message.content)
 
-#Command which downloads image with provided URL and saves it with requested filename
-@MyBot.command(aliases=["downloadimage", "dimage", "dim", "dimg", "d"], pass_context = True)
+
+# Command which downloads image with provided URL and saves it with requested filename
+@MyBot.command(aliases=["downloadimage", "dimage", "dim", "dimg"], pass_context=True)
 async def download_image(context, URL, output_file_char):
 
     output_file_format = ".png"
     output_file_name = output_file_char + output_file_format
 
-    #Image downloading part
-    img_info = requests.get(URL, stream = True)
+    # Image downloading part
+    img_info = requests.get(URL, stream=True)
     if img_info.status_code == 200:
         with open("images\\" + output_file_name, "wb") as output_file:
             shutil.copyfileobj(img_info.raw, output_file)
@@ -55,18 +56,30 @@ async def download_image(context, URL, output_file_char):
     channel = context.channel
     await channel.send("Image downloaded and saved...")
 
-@MyBot.command(aliases=["upload", "ul", "u", "uload", "l"], pass_context = True)
+
+# Command for uploading images from discord bot hosting server, from "\\images\\"" folder
+@MyBot.command(aliases=["upload", "ul", "u", "uload", "l"], pass_context=True)
 async def upload_image(context, requested_filename):
 
     channel = context.channel
-        
+
     try:
-        await MyBot.send_file(channel, "images\\" + requested_filename + ".png")
-        
+        await channel.send(file=discord.File("images\\" + requested_filename + ".png"))
+        await context.message.delete()
+
     except:
-        print("\nDebug: Requsted file does not exist!")
-        await channel.send("Requsted file does not exist!")
+        print("\nDebug: Requested file does not exist!")
+        await channel.send("Requested file does not exist!")
 
 
+# Command for deleting multiple messages
+@MyBot.command(aliases=["delete", "d", "del", "delete_all"], pass_context=True)
+async def delete_messages(context):
+
+    channel = context.channel
+    messages = await channel.history().flatten()
+
+    await channel.delete_messages(messages)
+ 
 
 MyBot.run(TOKEN)
